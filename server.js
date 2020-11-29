137173d0f6638fd158704d83ec4750b3da3587d0
Glitch (hello-express) <none>
2020-11-29 22:58:27 +0000

    🍐🍿 Checkpoint ./package.json:508702/870 ./server.js:508702/2183

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises,
   fsConst = require('fs').constants,
-  fetch = require('node-fetch');
+  fetch = require('node-fetch'),
+  calc = require('mathjs').evaluate;
 
 
 const app = require('express')();
@@ -68,6 +69,11 @@ app.get('/wtmoo', (req, res) => {
   res.send('eternal confusion');
 });
 
+app.get('/calc/:expression', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
+  res.send(calc(req.params.expression).toString());
+});
+
 app.get('/tld', (req, res) => {
   res.setHeader('content-type', 'text/plain');
   cache('https://data.iana.org/TLD/tlds-alpha-by-domain.txt', 'tlds', days(1))
@@ -126,78 +132,6 @@ const getHeaders = (req, res) => {
 app.get('/my/h', getHeaders);
 app.get('/my/headers', getHeaders);
 
-function lex(s) {
-  return s.match(/(?:[\d_]+(?:\.[\d_]+)?)|[-+*/^()]/g).map(m => /\d/.test(m) ? +m : m);
-}
-
-function tighterThanOrEq(op, cmp) {
-  let res;
-  switch (op) {
-    case '+': case '-': res = '+-*/^'.includes(cmp); break;
-    case '*': case '/': res = '*/^'.includes(cmp); break;
-    case '^': res = cmp === '^'; break;
-  }
-  return res;
-}
-
-function applyOp(stack, op) {
-  const r = stack.pop(),
-    l = stack.pop();
-  let res;
-  switch (op) {
-    case '+': res = l + r; break;
-    case '-': res = l - r; break;
-    case '*': res = l * r; break;
-    case '/': res = l / r; break;
-    case '^': res = l ** r; break;
-  }
-  stack.push(res);
-}
-
-function evaluate(l, i=0) {
-  const stack = [];
-  const ops = [];
-  let expectNumber = true;
-  let negateNext = false;
-  for (; i < l.length; i++) {
-    const tok = l[i];
-    if (expectNumber) {
-      if (typeof tok === 'number') {
-        stack.push(negateNext ? -tok : tok);
-        expectNumber = false;
-        negateNext = false;
-      } else if (tok === '-') {
-        negateNext = !negateNext;
-      } else if (tok === '(') {
-        let result = evaluate(l, ++i);
-        if (typeof result === 'undefined') { return; }
-        i = result[1];
-        stack.push(result[0]);
-      } else {
-        return; // invalid.
-      }
-    } else {
-      if (tok === ')') {
-        break;
-      } else if (typeof tok === 'number') {
-        return; // invalid.
-      } else {
-        while (ops.length > 0 && tighterThanOrEq(tok, ops[ops.length - 1])) {
-          applyOp(stack, ops.pop());
-        }
-        ops.push(tok);
-        expectNumber = true;
-      }
-    }
-  }
-  while (stack.length > 1) { applyOp(stack, ops.pop()); }
-  return [stack[0], ++i];
-}
-
-function isValidCalc(s) {
-  return /^\s*(?:[\d_]+(?:\.[\d_]+)?)(?:(?:\s*[-+*/^()])+\s*(?:[\d_]+(?:\.[\d_]+)?))*[\s)]*$/.test(s);
-}
-
 const redirects = [
   [['hs', 'haskell'], 'https://www.haskell.org/'],
   [['node', 'nodejs', 'node.js'], 'https://nodejs.org/'],
@@ -212,10 +146,6 @@ app.use((req, res, next) => {
   const url = decodeURIComponent(req.originalUrl.slice(1));
   console.log(url);
   if (url in redirectd) { res.redirect(redirectd[url]); return; }
-  if (isValidCalc(url)) {
-    const result = evaluate(lex(url));
-    if (typeof result !== 'undefined') { res.send(result[0].toString()); return; }
-  }
   next();
 });
 
