eb02c8d3177dad1458f1fd1b1dcfbfde10e75057
Glitch (hello-express) <none>
2020-11-30 12:33:15 +0000

    🎓⛑ Checkpoint ./server.js:508702/481

--- server.js
+++ server.js
@@ -121,6 +121,9 @@ const engines = [
   [['bing', 'b'], 'https://www.bing.com/search?q=%+q'],
   [['duckduckgo', 'ddg'], 'https://duckduckgo.com/?q=%+q'],
   [['ecosia', 'e'], 'https://www.ecosia.org/search?q=%+q'],
+  [['lmgtfy'], 'https://lmgtfy.app/#gsc.q=%q'],
+  // non text
+  [['lmgtfy images', 'lmgtfyi'], 'https://lmgtfy.app/#gsc.tab=1&gsc.q=%q']
   // videos
   [['youtube', 'yt'], 'https://www.youtube.com/results?search_query=%+q'],
   [['twitch', 'twitch.tv', 'ttv'], 'https://www.twitch.tv/search?term=%q'],
@@ -237,6 +240,10 @@ const getHeaders = (req, res) => {
 
 app.get('/my/h', getHeaders);
 app.get('/my/headers', getHeaders);
+
+app.get('/pl/syntax', (req, res) => {
+  res.redirect('http://rigaux.org/language-study/syntax-across-languages.html');
+});
 
 let home = '<meta name="viewport" content="width=device-width,initial-scale=1"><style>h1,h2,h3,h4,h5,h6{margin:0.5em 0;}ul{margin:0;padding-inline-start:20px;}</style><title>wtmoo is</title><a href="/">help</a><br><a href="https://glitch.com/edit/#!/wtmoo">source</a><br>';
 
