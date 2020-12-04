afa1c66abc6c64a2511b3f704ce94667d57670d4
Glitch (hello-express) <none>
2020-12-04 18:36:29 +0000

    🍃🌾 Checkpoint ./guides/compiler/foo.js:508702/1167

--- guides/compiler/foo.js
+++ guides/compiler/foo.js
@@ -1,7 +1,18 @@
-/* globals html, title, text, heading, subheading, block */
+/* globals html, title, text, heading, subheading, list, block, link */
 // NOTE: cb = pre(code())
 html(
 title`wtmoo is a compiler`,
-text``,
+heading`compilers`,
+text`hi this is how to make good compiler
+so you have your code as a string and you want to compile it.
+but wtmoo??? how???`,
+text`there are four main steps in compilation:`,
+list(
+link('lexing','./lexer'),
+link('parsing','./parser'),
+link('analyzing','./analyzer'),
+link('and generating.','./generator'),
+text(link('ide integration','./langserver'), ' is also part of the language but not really part of compilation'),
+),
 block()
 )
