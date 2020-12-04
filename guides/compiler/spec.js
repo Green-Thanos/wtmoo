44c44a0911090369e3adfa24cd8d8eee6090dc3d
Glitch (hello-express) <none>
2020-12-04 20:56:12 +0000

    🐢💸 Checkpoint ./guides/compiler/spec.js:508702/858

--- guides/compiler/spec.js
+++ guides/compiler/spec.js
@@ -1,5 +1,14 @@
 /* globals html, title, text, heading, subheading, list, block, c, link */
 html(
 title`wtmoo is wtc`,
-text`wtc is a simple language`
+text`wtc is a simple language. that is all`,
+heading`types`,
+list(
+// TODO: link
+text(c.type`Bool`, ` - boolean. this is an enum, we just need it so we can have equality in the stdlib`),
+text(c.type`Int`, ` - 64 bit integer`),
+text(c.type`Char`, ` - ascii character. utf-8 is a lot of added complexity`),
+text(c.type`List(T)`, ` - a generic type for ordered collections`),
+text(c.type`Dict(TKey, TVal)`, ` - a generic type for collections looked up by key`),
+)
 )
