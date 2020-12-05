fe294a29327ae60b8f9eb870c0a8333efa453d43
Glitch (hello-express) <none>
2020-12-05 20:09:50 +0000

    🍣🐴 Checkpoint ./guides/compiler/li18n/rust/node.js:508702/506

--- guides/compiler/li18n/rust/node.js
+++ guides/compiler/li18n/rust/node.js
@@ -1,14 +1,23 @@
 
 ````/* globals html, title, text, heading, subheading, list, block, c, lin`
 enum Literal {
+  Unit(),
   Boolean(bool),
   Integer(isize),
   Float(f64),
   String(Box<str>),
 }
 
+// TODO: mention size
 enum Node {
   Block { statements: Vec<Node> },
   Call { function: Node, arguments: Vec<Node> },
+  Literal { value: Literal },
+  If { condition: Node, body: Node },
+  IfElse { condition: Node, if_true: Node, if_false: Node },
+  For { variable: Node, array: Node, body: Node },
+  While { condition: Node, body: Node },
+  Match { expression: Node, body: Vec<Node> },
+  MatchArm { value: Node, body: Node },
 }
 ```
