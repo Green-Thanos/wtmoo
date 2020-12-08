d30b6508da6c7c6fc6338411269f968fe10e9cb9
Glitch (hello-express) <none>
2020-12-08 00:31:18 +0000

    🚝🍋 Checkpoint ./tools/my.js:508702/947 ./tools/rdns.js:508702/3855 ./server.js:508702/113

--- tools/my.js
+++ tools/my.js
@@ -1,6 +1,11 @@
+const dns = require("dns").promises;
+const dns_ = require("dns");
+const dnsErrors = {};
+for (const k in dns_) { if (/^[A-Z]+$/.test(k)) { dnsErrors[dns_[k]] = k; } }
+
 module.exports = function (app) {
   app.get('/my', (req, res) => {
-    res.send('<meta name="viewport" content="width=device-width,initial-scale=1"><title>what is my</title><a href="/my">help</a><br><a href="/my/h">headers</a><br><a href="/my/ip">ip</a><br><a href="/my/ua">user agent</a><br><a href="https://glitch.com/edit/#!/wim">source</a>');
+    res.send('<meta name="viewport" content="width=device-width,initial-scale=1"><title>what is my</title><a href="/my">help</a><br><a href="/my/h">headers</a><br><a href="/my/ip">ip</a><br><a href="/my/ua">user agent</a><br><a href="/ua">domain</a><br><a href="https://glitch.com/edit/#!/wim">source</a>');
   });
 
   const getUserAgent = (req, res) => {
@@ -36,4 +41,19 @@ module.exports = function (app) {
   app.get('/my/headers', getHeaders);
   app.get('/myh', getHeaders);
   app.get('/myheaders', getHeaders);
+  
+  const getDomain = async (req, res) => {
+    res.setHeader('content-type', 'text/plain');
+    const ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'].match(/[^,]+/)[0];
+    try {
+      const servers = await dns.reverse(ip);
+      res.send(servers.join('\n'));
+    } catch (e) {
+      res.status(404).send('error: ' + dnsErrors[e.code]);
+    }
+  }
+  app.get('/my/d', getDomain);
+  app.get('/my/domain', getDomain);
+  app.get('/myd', getDomain);
+  app.get('/mydomain', getDomain);
 }
---
+++ tools/rdns.js
@@ -0,0 +1,18 @@
+const dns = require("dns").promises;
+const dns_ = require("dns");
+const dnsErrors = {};
+for (const k in dns_) { if (/^[A-Z]+$/.test(k)) { dnsErrors[dns_[k]] = k; } }
+
+module.exports = function (app) {
+  async function reverseDNS(req, res) {
+    res.setHeader('content-type', 'text/plain');
+    try {
+      const servers = await dns.reverse(req.params.ip);
+      res.send(servers.join('\n'));
+    } catch (e) {
+      res.status(404).send('error: ' + dnsErrors[e.code]);
+    }
+  }
+  app.get('/rdns/:ip', reverseDNS);
+  app.get('/reversedns/:ip', reverseDNS);
+}
