8f8e631e423ae3907fb9e47db033dca49d2da9c6
Glitch (hello-express) <none>
2020-12-11 01:46:15 +0000

    😽🔨 Checkpoint ./tools/rph.js:508702/44

--- tools/rph.js
+++ tools/rph.js
@@ -36,7 +36,7 @@ module.exports = function ({app}) {
   rph({name: 'grian', color: 'e6c296', desc: 'best kotlin shill', desc2: 'kotlin shill (don\'t fite him)'});
   rph({name: 'rare', site: 'https://github.com/neverRare', color: 'ffd966', desc: 'very rare', desc2: 'extra super duper rare'});
   rph({name: 'phantz', site: 'https://github.com/TotallyNotChase', color: 'd07640', desc: 'also known as shirt (with a z)', desc2: 'martian pro gamer with a super shotgun (or two)'});
-  rph({name: 'aplet', site: 'https://github.com/Aplet123', color: 'f1e05a', desc: 'ja🅱️a applet', desc2: 'jaba applet'});
+  rph({name: 'aplet', site: 'https://aplet.me/', color: 'f1e05a', desc: 'ja🅱️a applet', desc2: 'jaba applet'});
   rph({name: 'arson', site: 'https://mee42.dev', domain: 'mee42.dev', color: 'b12a0b', desc: 'don\'t worry he\'s not actually an arsonist', desc2: 'not actually an arsonist'});
   rph({name: 'neko', site: 'https://skneko.moe', domain: 'skneko.moe', color: 'ad1457', desc: 'really a cat irl', desc2: 'a cat'});
   rph({name: 'phireh', color: 'a75aa8', desc: 'neko\'s friend idk', desc2: 'purple slime (he says it\'s a zergling but i\'m not sure i believe him)'});
