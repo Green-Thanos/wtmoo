fecc16caaca38b9efa438a4145c9a3f360a6d1ad
Glitch (hello-express) <none>
2021-01-09 13:47:01 +0000

    💒🎗 Checkpoint ./tools/modules.js:508702/2724

--- tools/modules.js
+++ tools/modules.js
@@ -1,26 +1,59 @@
-const turndown = new require('turndown')(),
-  fetch = require('node-fetch');
+function lazy(name, fn) {
+  Object.defineProperty(global, name, { get: () => { const val = fn(); Object.defineProperty(global, name, val); return val} });
+}
+
+const fetch = require('node-fetch');
+
+/* globals turndown */
+lazy('turndown', () => {
+  const turndownService = new require('turndown')()
+  return turndownService.turndown.bind(turndownService);
+});
 
 module.exports = function ({app}) {
   async function crateInfo(crate) {
-    return await fetch('https://crates.io/api/v1/crates/' + crate);
+    const req = await fetch('https://crates.io/api/v1/crates/' + crate);
+    return await req.json();
   }
+
   app.get('/m/crates/:crate', async (req, res) => {
     res.setHeader('content-type', 'text/plain');
     const info = await crateInfo(req.params.crate);
+    const readmeReq = await fetch(info.crate.readme_path);
+    const readme = await readmeReq.text();
+    const result = `\
+description: ${info.crate.description}
+version: ${info.crate.newest_version}
+downloads: ${info.crate.downloads}
+`;
+    result += turndown(readme);
+    res.send(result);
   });
 
   app.get('/m/crates/:crate/readme', async (req, res) => {
     res.setHeader('content-type', 'text/plain');
     const info = await crateInfo(req.params.crate);
+    const readmeReq = await fetch(info.crate.readme_path);
+    const readme = await readmeReq.text();
+    res.send(turndown(readme));
   });
 
   app.get('/m/crates/:crate/features', async (req, res) => {
     res.setHeader('content-type', 'text/plain');
     const info = await crateInfo(req.params.crate);
     const result = '';
-    for (const key in info.features) {
-      result += `${key} = [${info.features[key].map(s => JSON.stringify(s)).join(', ')}]\n`;
+    for (const key in info.crate.features) {
+      result += `${key} = [${info.crate.features[key].map(s => JSON.stringify(s)).join(', ')}]\n`;
+    }
+    res.send(result);
+  });
+
+  app.get('/m/crates/:crate/versions', async (req, res) => {
+    res.setHeader('content-type', 'text/plain');
+    const info = await crateInfo(req.params.crate);
+    const result = '';
+    for (const key in info.crate.features) {
+      result += `${key} = [${info.crate.features[key].map(s => JSON.stringify(s)).join(', ')}]\n`;
     }
     res.send(result);
   });
